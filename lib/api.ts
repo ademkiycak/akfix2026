65616eb07d7a22a05b596a575974b8e2da608382
Adem Kıyçak <ademkiycak@Adem-MacBook-Pro.local>
2026-02-27 08:09:18 +0300

    fix: update api file

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,7 @@
 export async function fetchData<T>(url: string): Promise<T> {
-  const res = await fetch(url)
+  const res = await fetch(url, {
+    next: { revalidate: 60 },
+  })
   if (!res.ok) throw new Error(`Fetch hatası: ${res.status}`)
   return res.json()
 }
